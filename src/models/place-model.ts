48fcb9d69d691231a812a34d1aa66ffe985e1a89
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-07 23:44:42 +0200

    methods add, and rem are added & some changes are made

--- src/models/place-model.ts
+++ src/models/place-model.ts
@@ -1,22 +1,64 @@
-import { model, Schema } from 'mongoose';
+import { Model, model, Schema, startSession } from 'mongoose';
 
-const placeSchema = new Schema(
+import User from './user-model';
+import { PlaceDocMethodsI, PlaceSchemaI } from './place-interfaces';
+import ResponseError from './response-error';
+
+const placeSchema = new Schema<PlaceSchemaI, Model<PlaceSchemaI>, PlaceDocMethodsI>(
     {
-        title: String,
-        description: String,
-        address: String,
-        imgURL: String,
+        title: { type: String, required: true },
+        description: { type: String, required: true },
+        address: { type: String, required: true },
+        imgURL: { type: String, required: true },
         location: {
-            _id: false,
-            lat: Number,
-            lng: Number,
+            lat: { type: Number, required: true },
+            lng: { type: Number, required: true },
         },
-        creator: Schema.Types.ObjectId,
+        creator: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
     },
     {
         timestamps: true,
+        toObject: { getters: true },
     }
 );
+
+placeSchema.methods.add = async function () {
+    const session = await startSession();
+    session.startTransaction();
+
+    const creator = await User.findById(this.creator, undefined, { session }).exec();
+
+    if (!creator) {
+        throw new ResponseError("there's no user with the entered creator!", 404);
+    }
+
+    await this.save({ session });
+
+    await creator.set('session', session).addPlace(this);
+
+    await session.commitTransaction();
+
+    return this.toObject();
+};
+
+placeSchema.methods.rmv = async function () {
+    const session = await startSession();
+    session.startTransaction();
+
+    const creator = await User.findById(this.creator, undefined, { session }).exec();
+
+    if (!creator) {
+        throw new ResponseError('place creator not found!', 404);
+    }
+
+    await creator.set('session', session).deletePlace(this);
+
+    await this.delete({ session });
+
+    await session.commitTransaction();
+
+    return null;
+};
 
 const Place = model('Place', placeSchema);
 
