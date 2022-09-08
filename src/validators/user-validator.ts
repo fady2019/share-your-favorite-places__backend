d4f12f8582ca2131af64380cb7a65fdd9568f967
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-08 19:34:45 +0200

    refactoring user info validator

--- src/validators/user-validator.ts
+++ src/validators/user-validator.ts
@@ -1,26 +1,25 @@
 import { body } from 'express-validator';
 
-export const userValidator = [
-    body('name')
+const nameValidator = (field: string) =>
+    body(field)
         .not()
         .isEmpty()
         .withMessage('user name is required!')
         .isAlpha(undefined, { ignore: [' '] })
-        .withMessage('user name should consist of English alphabetic only!'),
+        .withMessage('user name should consist of English alphabetic only!');
 
-    body('email')
-        .normalizeEmail()
-        .not()
-        .isEmpty()
-        .withMessage('user email is required!')
-        .isEmail()
-        .withMessage('invalid user email!'),
+const emailValidator = (field: string) =>
+    body(field).normalizeEmail().not().isEmpty().withMessage('user email is required!').isEmail().withMessage('invalid user email!');
 
-    body('password')
+const passwordValidator = (field: string) =>
+    body(field)
         .isLength({ min: 8 })
         .withMessage('user password should be at least 8 characters!')
         .isStrongPassword({ minLowercase: 1, minNumbers: 1, minSymbols: 1, minUppercase: 1 })
-        .withMessage(
-            'user password is week, enter a password contains lowercase and uppercase letters, numbers, and symbols!'
-        ),
-];
+        .withMessage('user password is week, enter a password contains lowercase and uppercase letters, numbers, and symbols!');
+
+export const userSignupValidator = [nameValidator('name'), emailValidator('email'), passwordValidator('password')];
+
+export const userChangeEmailValidator = [emailValidator('newEmail')];
+
+export const userChangePasswordValidator = [passwordValidator('newPassword')];
