2c9b636968aa6613224983b612d7921611b22434
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-10 22:40:29 +0200

    image uploader middleware is added when creating and updating places

--- src/routes/places-routes.ts
+++ src/routes/places-routes.ts
@@ -9,6 +9,7 @@ import {
 } from '../controllers/places-controllers';
 
 import { placeValidator } from '../validators/place-validator';
+import { imageUploader } from '../middlewares/image-uploader-middleware';
 
 const router = Router();
 
@@ -18,7 +19,7 @@ router.get('/user/:userId', getUserPLaces);
 
 // POST /places/new
 // create a new place
-router.post('/new', placeValidator, createPlace);
+router.post('/new', imageUploader('places').single('image'), placeValidator, createPlace);
 
 // GET /places/:placeId
 // get the place with id = placeId
@@ -26,7 +27,7 @@ router.get('/:placeId', getPlace);
 
 // PATCH /places/:placeId
 // update the place with id = placeId
-router.patch('/:placeId', placeValidator, updatePlace);
+router.patch('/:placeId', imageUploader('places').single('image'), placeValidator, updatePlace);
 
 // DELETE places/:placeId
 // delete the place with id = placeId
