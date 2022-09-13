5ac0e5e9944b79d9e75d025370e9e22f4a0b668c
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-13 11:14:46 +0200

    utilities of generating and refreshing tokens are added

---
+++ src/utilities/token-utility.ts
@@ -0,0 +1,62 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+import ResponseError from '../models/response-error';
+
+const isExpired = (exp: number) => {
+    return Date.now() >= exp * 1000;
+};
+
+const clearPayload = (payload: JwtPayload) => {
+    const res = { ...payload };
+
+    delete res.aud;
+    delete res.exp;
+    delete res.iat;
+    delete res.iss;
+    delete res.jti;
+    delete res.nbf;
+    delete res.sub;
+
+    return res;
+};
+
+export const extractToken = (auth: any) => {
+    return auth?.split(' ')[1];
+};
+
+export const generateToken = (payload: any) => {
+    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY as string, { expiresIn: '1h' });
+    const exp = (jwt.decode(token) as any).exp;
+    return {
+        id: token,
+        expireAt: exp * 1000,
+    };
+};
+
+export const refreshToken = (auth: any) => {
+    try {
+        const token = extractToken(auth);
+
+        const payload: any = jwt.decode(token);
+
+        if (!payload) {
+            throw new ResponseError('token is required!', 400);
+        }
+
+        let newToken;
+
+        if (isExpired(payload.exp)) {
+            const userPayload = clearPayload(payload);
+            newToken = generateToken(userPayload);
+        } else {
+            newToken = {
+                id: token,
+                expireAt: payload.exp * 1000,
+            };
+        }
+
+        return newToken;
+    } catch (error) {
+        throw error;
+    }
+};
