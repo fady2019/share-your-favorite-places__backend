3a72109d9ea4c0277ff362a576e3955ac04efbdf
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-13 11:25:46 +0200

    token checker middleware has been used before some routes of place

--- src/routes/places-routes.ts
+++ src/routes/places-routes.ts
@@ -10,27 +10,35 @@ import {
 
 import { placeValidator } from '../validators/place-validator';
 import { imageUploader } from '../middlewares/image-uploader-middleware';
+import { tokenChecker } from '../middlewares/token-checker-middleware';
 
 const router = Router();
 
 // GET /places/user/:userId
 // get the places of a user that has id = userId
+// Need No Authentication
 router.get('/user/:userId', getUserPLaces);
-
-// POST /places/new
-// create a new place
-router.post('/new', imageUploader('places').single('image'), placeValidator, createPlace);
 
 // GET /places/:placeId
 // get the place with id = placeId
+// Need No Authentication
 router.get('/:placeId', getPlace);
+
+router.use(tokenChecker());
+
+// POST /places/new
+// create a new place
+// Need Authentication
+router.post('/new', imageUploader('places').single('image'), placeValidator, createPlace);
 
 // PATCH /places/:placeId
 // update the place with id = placeId
+// Need Authentication
 router.patch('/:placeId', imageUploader('places').single('image'), placeValidator, updatePlace);
 
 // DELETE places/:placeId
 // delete the place with id = placeId
+// Need Authentication
 router.delete('/:placeId', deletePLace);
 
 export default router;
