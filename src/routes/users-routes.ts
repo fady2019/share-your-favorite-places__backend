3303fb38a583f112edd2f7749f29d7ce3a47c663
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-13 11:31:05 +0200

    some routes have been adjusted and token checker middleware has been added

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,36 +1,61 @@
 import { Router } from 'express';
 
-import { changeEmail, changeName, changePassword, deleteAccount, getUsers, postLogin, postSignup } from '../controllers/users-controllers';
-import { userChangeEmailValidator, userChangeNameValidator, userChangePasswordValidator, userSignupValidator } from '../validators/user-validator';
+import {
+    changeEmail,
+    changeName,
+    changePassword,
+    deleteAccount,
+    getUsers,
+    postLogin,
+    postSignup,
+} from '../controllers/users-controllers';
+
+import {
+    userChangeEmailValidator,
+    userChangeNameValidator,
+    userChangePasswordValidator,
+    userSignupValidator,
+} from '../validators/user-validator';
+
+import { tokenChecker } from '../middlewares/token-checker-middleware';
 
 const router = Router();
 
 // GET /users/
 // get users
+// Need No Authentication
 router.get('/', getUsers);
 
 // POST /users/signup
 // signup a user
+// Need No Authentication
 router.post('/signup', userSignupValidator, postSignup);
 
 // POST /users/login
 // login a user
+// Need No Authentication
 router.post('/login', postLogin);
 
-//PARCH /users/change/email/:userId
+router.use(tokenChecker());
+
+//PARCH /users/change/email
 // change user email (password and new email are required)
-router.patch('/change/email/:userId', userChangeEmailValidator, changeEmail);
+// Need Authentication
+router.patch('/change/email', userChangeEmailValidator, changeEmail);
 
-//PARCH /users/change/password/:userId
+//PARCH /users/change/password
 // change user password (password and new password are required)
-router.patch('/change/password/:userId', userChangePasswordValidator, changePassword);
+// Need Authentication
+router.patch('/change/password', userChangePasswordValidator, changePassword);
 
-// PATCH /users/change/name/:userId
+// PATCH /users/change/name
 // change user name
-router.patch('/change/name/:userId', userChangeNameValidator, changeName);
+// Need Authentication
+router.patch('/change/name', userChangeNameValidator, changeName);
 
-// POST /users/delete/:userId
+// POST /users/delete
 // delete a user account
-router.post('/delete/:userId', deleteAccount);
+// Need Authentication
+router.post('/delete', deleteAccount);
 
 export default router;
