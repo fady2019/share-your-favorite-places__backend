2445786412d28d0316e668d11e70f105237ca895
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-13 15:32:11 +0200

    delete file utility has been adjusted

--- src/utilities/file-utility.ts
+++ src/utilities/file-utility.ts
@@ -4,10 +4,23 @@ import { NextFunction } from 'express';
 
 import { rootPath } from './path-utility';
 
+const deletableDirs = ['media'];
+
 export const deleteFile = (filePath: string, next?: NextFunction) => {
+    const filePathParts = path
+        .normalize(filePath)
+        .split(path.sep)
+        .filter((part) => part.trim() !== '');
+
+    const filePathPrefix = filePathParts[0];
+
+    if (filePathParts.length < 2 || deletableDirs?.indexOf(filePathPrefix) === -1) {
+        return;
+    }
+
     const absPath = path.join(rootPath, filePath);
 
-    fs.unlink(absPath, (error) => {
+    fs.rm(absPath, { recursive: true, force: true }, (error) => {
         if (next) {
             next(error);
         }
