39eba4e3720c83b2f6faf31b7dfda434a013d5ed
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-17 19:38:11 +0200

    a utility function has been added to update a token

--- src/utilities/token-utility.ts
+++ src/utilities/token-utility.ts
@@ -33,6 +33,33 @@ export const generateToken = (payload: any) => {
     };
 };
 
+export const updateTokenPayload = (auth: any, newPayload: any) => {
+    try {
+        const token = extractToken(auth);
+
+        const payload: any = jwt.decode(token);
+
+        if (!payload) {
+            throw new ResponseError('token is required!', 400);
+        }
+
+        const expiresIn = payload.exp * 1000 - new Date().getTime();
+
+        const updatedToken = jwt.sign(newPayload, process.env.JWT_SECRET_KEY as string, {
+            expiresIn: (expiresIn < 0 ? 0 : expiresIn) + 'ms',
+        });
+
+        const exp = (jwt.decode(token) as any).exp; 
+        
+        return {
+            id: updatedToken,
+            expireAt: exp * 1000,
+        };
+    } catch (error) {
+        throw error;
+    }
+};
+
 export const refreshToken = (auth: any) => {
     try {
         const token = extractToken(auth);
