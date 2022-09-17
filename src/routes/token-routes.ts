1e187d410b113c760ec1ade97bfdecfc60812c18
fady2019 <60940882+fady2019@users.noreply.github.com>
2022-09-17 19:40:45 +0200

    get user info when refreshing a token

--- src/routes/token-routes.ts
+++ src/routes/token-routes.ts
@@ -1,12 +1,33 @@
 import { Router } from 'express';
+
+import ResponseError from '../models/response-error';
+import { tokenChecker } from '../middlewares/token-checker-middleware';
 import { refreshToken } from '../utilities/token-utility';
+import User from '../models/user-model';
 
 const router = Router();
 
 // token/refresh
-router.post('/refresh', (req, res, _next) => {
+router.post('/refresh', tokenChecker(true), (req, res, next) => {
+    const user = req.user;
+
     const token = refreshToken(req.headers.authorization);
-    res.json({ token });
+
+    User.findOne({ _id: user?.id, email: user?.email })
+        .select('-password -places')
+        .exec()
+        .then((user) => {
+            if (!user) {
+                throw new ResponseError('user not exists, please login again with your credentials!', 400);
+            }
+
+            res.status(200).json({
+                message: 'refreshed successfully!',
+                user: user.toObject({ virtuals: ['id'] }),
+                token,
+            });
+        })
+        .catch((error) => next(error));
 });
 
 export default router;
